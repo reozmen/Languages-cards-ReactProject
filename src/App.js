34b74d9c5d6d2b9722d5cda41c23fe62b479da5c
reozmen <recepozmen41@gmail.com>
2023-04-30 01:27:51 +0300

    değişiklikler eklendi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
-import './App.css';
-import Header from "./components/header/Header"
-import Card from "./components/card/Card"
-
-
+import "./App.css";
+import Header from "./components/header/Header";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="main-container">
-      <Header/>
-      <Card/>
+      <Header />
+      <Card />
     </div>
   );
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,23 +1,19 @@
-import {languages} from "../../helpers/data"
-import Item from "../../components/item/Item"
-import "./Card.css"
+import { languages } from "../../helpers/data";
+import Item from "../../components/item/Item";
+import "./Card.css";
 
 const Card = () => {
-    return (
-        <div className = "cards-area-container">
-            <div className="bars"></div>
-            <h5 className="language-title">Languages</h5>
-            <div className="cards-container">
-                { languages.map( (item,index) => {
-                return (
-                    <Item card = {item} key={index}/>
-                )
-            }
-
-            )}
-           </div>
-        </div>
-    )
-}
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h5 className="language-title">Languages</h5>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
