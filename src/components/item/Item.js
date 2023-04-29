a335b7d9b477b96cbfeb4e8623b1cbd914ab02a9
reozmen <recepozmen41@gmail.com>
2023-04-30 01:22:01 +0300

    css changed and settimeout function added

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -3,23 +3,29 @@ import "./Item.css";
 
 const Item = ({ card }) => {
   const [showLogo, setShowLogo] = useState(true);
-  
+
+  if (!showLogo) {
+    setTimeout(() => {
+      setShowLogo(true);
+    }, 3000);
+  }
   return (
     <div className="card" onClick={() => setShowLogo(!showLogo)}>
-    {showLogo ? (
+      {showLogo ? (
         <div>
           <img className="card-logo" src={card.img} alt="logo" />
           <h3 className="card-title">{card.name}</h3>
         </div>
       ) : (
-        
         <ul className="list">
+          <p>Usage Areas:</p>
           {card.options.map((element, index) => {
             return <li key={index}>{element}</li>;
           })}
-        </ul>)}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Item;  
+export default Item;
